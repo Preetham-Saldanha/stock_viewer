969d4c7fe8a58315dd5d82ff0fe67f28d1859a05
preetham-saldanha <preethamsaldanha26@gmail.com>
2023-06-17 14:14:20 +0530

    refactored js

--- frontend/index.js
+++ frontend/index.js
@@ -7,92 +7,78 @@
 
 class App {
   constructor() {
-    this.stockData = {};
+    this.stockData = [];
+
+    this.createInitialTable();
   }
 
-  createInitialTable() {}
-}
+  createInitialTable() {
+    console.log("initiating table...");
+    document.addEventListener("DOMContentLoaded", async () => {
+      const response = await fetch("http://localhost:3000/api/home");
+      const data = await response.json();
+      this.stockData = data.top_15_entries_with_stock_data;
+      console.log("1", this.stockData);
+      const tableBody = document.getElementById("stockTableBody");
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const response = await fetch("http://localhost:3000/api/home");
-  const data = await response.json();
+      this.stockData.forEach((entry, index) => {
+        const {
+          "Security Code": securityCode,
+          "Issuer Name": issuerName,
+          "Security Id": securityId,
+          "Sector Name": sectorName,
+          Open,
+          Close,
+          High,
+          Low,
+          "Open-Close (%)": openClosePercentage,
+          "Market Cap (Lakhs)": marketCap,
+        } = entry;
 
-  const stockData = data.top_15_entries_with_stock_data;
+        const row = document.createElement("tr");
 
-  const tableBody = document.getElementById("stockTableBody");
+        row.innerHTML = `
+            <td>${index + 1}</td>
+            <td ><a href="stock.html" class="company-link issuer">${issuerName}</a></td>
+            <td>${securityId}</td>
+            <td>${sectorName}</td>
+            <td>${Open}</td>
+            <td>${Close}</td>
+            <td>${High}</td>
+            <td>${Low}</td>
+            <td>${openClosePercentage}%</td>
+            <td>${marketCap}</td>
+          `;
 
-  stockData.forEach((entry, index) => {
-    const {
-      "Security Code": securityCode,
-      "Issuer Name": issuerName,
-      "Security Id": securityId,
-      "Sector Name": sectorName,
-      Open,
-      Close,
-      High,
-      Low,
-      "Open-Close (%)": openClosePercentage,
-      "Market Cap (Lakhs)": marketCap,
-    } = entry;
+        tableBody.appendChild(row);
+      });
 
-    const row = document.createElement("tr");
-    const currData = {
-      issuerName,
-      sectorName,
-      securityCode,
-      Open,
-      Close,
-      High,
-      Low,
-      openClosePercentage: openClosePercentage,
-      marketCap: marketCap,
-    };
-    row.innerHTML = `
-        <td>${index + 1}</td>
-        <td ><a href="stock.html" class="company-link issuer">${issuerName}</a></td>
-        <td>${securityId}</td>
-        <td>${sectorName}</td>
-        <td>${Open}</td>
-        <td>${Close}</td>
-        <td>${High}</td>
-        <td>${Low}</td>
-        <td>${openClosePercentage}%</td>
-        <td>${marketCap}</td>
-      `;
-
-    tableBody.appendChild(row);
-  });
-
-  const companyLinks = document.querySelectorAll(".company-link");
-  companyLinks.forEach((link, index) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      // Get the data for the clicked row
-      const rowData = stockData[index];
-
-      // Serialize the data as a query string
-      const queryString = new URLSearchParams(rowData).toString();
-
-      // Generate the URL for the new page with the query string
-      const url = `stock.html?${queryString}`;
-
-      // Redirect to the new page
-      window.location.href = url;
+      this.addCompanyLinkEventListeners();
     });
-  });
-});
-
-class Test {
-  constructor() {
-    this.greet = "hello";
-    this.run();
   }
 
-  run() {
-    console.log("running...");
+  addCompanyLinkEventListeners() {
+    console.log("adding links...");
+    const companyLinks = document.querySelectorAll(".company-link");
+    companyLinks.forEach((link, index) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        // Get the data for the clicked row
+        const rowData = this.stockData[index];
+
+        // Serialize the data as a query string
+        const queryString = new URLSearchParams(rowData).toString();
+
+        // Generate the URL for the new page with the query string
+        const url = `stock.html?${queryString}`;
+
+        // Redirect to the new page
+        window.location.href = url;
+      });
+    });
   }
 }
+new App();
 
-new Test();
 // Inside the DOMContentLoaded event listener
