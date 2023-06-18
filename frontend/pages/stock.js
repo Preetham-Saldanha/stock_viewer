4b5fb83c2140a45e2c7208467048df17922aec7b
preetham-saldanha <preethamsaldanha26@gmail.com>
2023-06-19 02:03:22 +0530

    completed watchlist feature

--- frontend/pages/stock.js
+++ frontend/pages/stock.js
@@ -1,4 +1,5 @@
 // Data from the provided array
+let stockData;
 document.addEventListener("DOMContentLoaded", async () => {
   const urlParams = new URLSearchParams(window.location.search);
 
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const heading = document.createElement("h2");
     heading.classList.add("stock-detail-heading");
+
     heading.textContent = key;
 
     const value = document.createElement("p");
@@ -34,6 +36,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   // ... retrieve other properties as needed
+  stockData = { ...data };
   console.log(urlParams, securityId, data);
   // Use the data to populate and replace the content on the page
   // document.getElementById("securityId").textContent = securityId;
@@ -67,29 +70,124 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 });
 
-class SearchBar {
+function debounce(func, time) {
+  let initial;
+
+  function exec() {
+    initial = setTimeout(() => {
+      func();
+    }, time);
+  }
+
+  // clearTimeout(initial);
+  return function () {
+    if (initial !== undefined) clearInterval(initial);
+    exec();
+  };
+}
+
+class Stock {
   constructor() {
-    this.searchBar = document.querySelector('#search-bar input[type="text"]');
-    this.searchOverlay = document.getElementById("search-overlay");
-    this.onSearchActive();
-    this.onSearchInactive();
+    this.handleAddToPortfolio();
+    this.handleAddToWatchlist();
+  }
+  addToStorage(storePlace, stockData) {
+    console.log(stockData);
+    const currentWL = window.localStorage.getItem(storePlace);
+    // console.log(currentWL, "curre");
+    if (!currentWL) {
+      window.localStorage.setItem(
+        storePlace,
+        JSON.stringify([{ [stockData["Security Code"]]: stockData }])
+      );
+    } else {
+      let flag = false;
+      const parsedWL = JSON.parse(currentWL);
+      for (let item of parsedWL) {
+        if (item[stockData["Security Code"]]) {
+          console.log("already present in ", storePlace);
+          flag = true;
+          break;
+        }
+      }
+
+      // if estock is not already present in wl
+      if (!flag) {
+        window.localStorage.setItem(
+          storePlace,
+          JSON.stringify([
+            ...parsedWL,
+            { [stockData["Security Code"]]: stockData },
+          ])
+        );
+      }
+    }
+  }
+  // add this methpd later
+  handleEvent(message) {}
+
+  handlePopup(message, link) {
+    // Create the modal container
+    const modalContainer = document.querySelector(".modal");
+    const popupOverlay = document.querySelector(".popup-overlay");
+
+    // Create the heading element
+    const heading = document.createElement("h2");
+    heading.textContent = message;
+
+    // Create the button container
+    const buttonContainer = document.createElement("div");
+
+    // Create the "Yes" button
+    const yesButton = document.createElement("button");
+    yesButton.classList.add("blue-button");
+    yesButton.textContent = "Yes";
+    yesButton.onclick = () => {
+      window.location.href = link;
+    };
+    // Create the "Later" button
+    const laterButton = document.createElement("button");
+    laterButton.classList.add("red-button");
+    laterButton.textContent = "Later";
+    laterButton.onclick = () => {
+      modalContainer.innerHTML = "";
+      popupOverlay.style.display = "none";
+      modalContainer.style.display = "none";
+    };
+    // Append the buttons to the button container
+    buttonContainer.appendChild(yesButton);
+    buttonContainer.appendChild(laterButton);
+
+    // Append the heading and button container to the modal container
+    modalContainer.appendChild(heading);
+    modalContainer.appendChild(buttonContainer);
+
+    popupOverlay.style.display = "block";
+    modalContainer.style.display = "flex";
   }
 
-  onSearchActive() {
-    this.searchBar.addEventListener("click", () => {
-      this.searchOverlay.style.display = "block";
-      document.body.style.overflow = "hidden";
+  handleAddToWatchlist() {
+    const wlButton = document.querySelector("#addToWL");
+
+    wlButton.addEventListener("click", () => {
+      this.addToStorage("wl", stockData);
+      this.handlePopup(
+        "Do you want to look into your WatchList?",
+        "watchList.html"
+      );
     });
-
-    console.log("serach js runs");
   }
 
-  onSearchInactive() {
-    this.searchOverlay.addEventListener("click", () => {
-      this.searchOverlay.style.display = "none";
-      document.body.style.overflow = "visible";
+  handleAddToPortfolio() {
+    const pfButton = document.querySelector("#addToPF");
+
+    pfButton.addEventListener("click", () => {
+      this.addToStorage("pf", { ...stockData });
+      this.handlePopup(
+        "Do you want to look into your Portfolio?",
+        "portfolio.html"
+      );
     });
   }
 }
-
-new SearchBar();
+new Stock();
